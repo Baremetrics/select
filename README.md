5166d7dc08256d45b96cc4c813b411878f4198a9
Tyler van der Hoeven <hi@tyvdh.com>
2015-09-23 11:25:31 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# [Baremetrics](https://baremetrics.com/) Date Range Picker
+_[Baremetrics](https://baremetrics.com) provides one-click analytics & insights for Stripe. **[Get started today!](https://baremetrics.com)**_
+
+---
+
+The Baremetrics custom selection picker a glorified select box solution. It's super flexible and allows many different types and ranges of complex selections while at the end of the day generating a very simple, concise JSON object which you can use for whatever.
+
+Design by [Chris Meeks](https://dribbble.com/ChrisMeeks)  
+Code by [Tyler van der Hoeven](https://github.com/tyvdh)
+
+[View a demo](http://baremetrics.github.io/select/)  
+[View in a live production app](https://demo.baremetrics.com/sales/search)
+
+// Screenshots
+![]()
+////
+
+## Installing
+
+
+### Params
+
+---
+
+## Developing
+
+I've included my signature gulpfile too so be sure and take a look at that as well.
+
+```bash
+$ cd <project directory>
+$ npm install
+$ gulp
+```
+
+I also use [pow](http://pow.cx/) and the [powder gem](https://github.com/Rodreegez/powder) to run my local dev environments but however you plan on wrangling that the gulpfile turns on a livereload server so as long as you have the files serving somehow any changes you make will show up instantly.
+
+## Dependencies
+- [jQuery](https://jquery.com/)
---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+    ghPages = require('gulp-gh-pages'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    livereload = require('gulp-livereload'),
+    newer = require('gulp-newer');
+
+
+// ROOT TASKS // ---------------------------------------------------------
+// Main style task  
+gulp.task('css', function() {
+  return gulp.src('dev/sass/application.scss')
+    .pipe(sass())
+    .on('error', handleError)
+    .pipe(autoprefixer({cascade: false})) // auto prefix
+    .pipe(minifycss()) // minify everything
+    .pipe(gulp.dest('public/css'));
+});
+
+// Main Javascript task
+gulp.task('js', function() {  
+  return gulp.src('dev/js/**/*.js')
+    .pipe(newer('public/js'))
+    .pipe(uglify())
+    .on('error', handleError)
+    .pipe(gulp.dest('public/js'));
+});
+
+// Publish github page
+gulp.task('deploy', function() {
+  return gulp.src('public/**/*')
+    .pipe(ghPages());
+});
+
+
+// FUNCTIONS // ---------------------------------------------------------
+// Watch function
+gulp.task('watch', function() {
+  gulp.start('js', 'css');
+
+  gulp.watch('dev/sass/**/*.scss', ['css']);
+  gulp.watch('dev/js/**/*.js', ['js']);
+  gulp.watch('dev/img/**/*.{jpg,jpeg,png,gif,svg,ico}', ['img']);
+ 
+  livereload.listen();
+  gulp.watch(['public/*.html', 'public/js/**/*.js', 'public/img/**/*.{jpg,jpeg,png,gif,svg,ico}', 'public/css/*.css']).on('change', livereload.changed);
+});
+
+// Default function
+gulp.task('default', ['watch']);
+
+// Error reporting function
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
